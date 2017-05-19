959d4e149068a9395963f8e06f2c5a39bf2d9a4b
Victor A. Santos <victoraur.santos@gmail.com>
2017-05-19 00:04:01 -0300

    delete useless class; add SQLite sql database

---
+++ data/create_db_sqlite.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE escola;
+
+use escola;
+
+CREATE TABLE alunos(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `uuid` VARCHAR(45) NOT NULL,
+    `nome` VARCHAR(255) NOT NULL,
+    `endereco` VARCHAR(255),
+    `turma_id` INT NOT NULL,
+    PRIMARY KEY(id, uuid)
+);
+
+CREATE TABLE professores(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `uuid` VARCHAR(45) NOT NULL,
+    `nome` VARCHAR(255) NOT NULL,
+    `endereco` VARCHAR(255),
+    `salario` DECIMAL NOT NULL,
+    `escola_id` INT NOT NULL,
+    PRIMARY KEY(id, uuid)
+);
+
+CREATE TABLE turmas(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `uuid` VARCHAR(45) NOT NULL,
+    `serie` VARCHAR(255) NOT NULL,
+    `escola_id` INT NOT NULL,
+    PRIMARY KEY(id, uuid)
+);
+
+CREATE TABLE escolas(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(255) NOT NULL,
+    `endereco` VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE alunos ADD FOREIGN KEY (turma_id) REFERENCES turmas(id) ON UPDATE cascade ON DELETE cascade;
+
+ALTER TABLE professores ADD FOREIGN KEY (escola_id) REFERENCES escolas(id) ON UPDATE cascade ON DELETE cascade;
+
+ALTER TABLE turmas ADD FOREIGN KEY (escola_id) REFERENCES escolas(id) ON UPDATE cascade ON DELETE cascade;
